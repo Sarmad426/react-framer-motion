75fb212d9f1c0cbf760c87525ae3a9c98675ee29
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-08-12 20:22:57 +0500

    Updated scroll animations

--- src/components/viewAnimations.tsx
+++ src/components/viewAnimations.tsx
@@ -1,36 +1,33 @@
-import {motion, useInView} from 'framer-motion'
-import { useRef } from 'react'
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
-export const ViewAnimations = () =>{
+export const ViewAnimations = () => {
+  const ref = useRef(null);
 
-    const ref = useRef(null)
-
-    // Remove {once:true} for continuous animation
-    const isInView = useInView(ref,{once:true});
-    return (
-        <div >
-            <div
-            style={{height:'120vh'}}
-            >
-                Scroll Down
-            </div>
-            <motion.div
-            style={{height:'10rem',width:'20rem',background:'var(--primary-color)'}}
-            initial={{opacity:0}}
-            whileInView={{opacity:1}}
-            transition={{duration:1}}
-            />
-            <br /> <br />
-            <div
-            ref={ref}
-            style={
-                {
-                height:'100vh',
-                background: isInView ? 'blue':'red',
-                transition:'1s background'
-            }
-        }
-            />
-        </div>
-    )
-}
+  // Add {once:true} for continuous animation
+  const isInView = useInView(ref);
+  return (
+    <div>
+      <div style={{ height: "120vh" }}>Scroll Down</div>
+      <motion.div
+        style={{
+          height: "10rem",
+          width: "20rem",
+          background: "var(--primary-color)",
+        }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      />
+      <br /> <br />
+      <div
+        ref={ref}
+        style={{
+          height: "100vh",
+          background: isInView ? "blue" : "red",
+          transition: "1s background",
+        }}
+      />
+    </div>
+  );
+};
