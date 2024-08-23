fd6fa66a07ee8f9e24661c49dfd2228838b63de4
Sarmad rafique <sarmadrafique040@gmail.com>
2024-08-23 07:10:20 +0500

    Updated navbar styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,27 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Navbar = () => {
+  const location = useLocation();
+
+  const getLinkClass = (path: string) => {
+    const isActive = location.pathname === path;
+    return isActive ? "active" : "";
+  };
+
+  console.log("Pathname", location.pathname);
+
   return (
-    <nav className="sticky top-0">
-      <Link to="basics" className="button">
+    <nav className="flex flex-col gap-4 absolute left-0 top-0 p-6 h-screen dark:bg-gray-700 bg-gray-100">
+      <Link to="/basics" className={`button ${getLinkClass("/basics")}`}>
         Basics
       </Link>
-      <Link to="gestures" className="button">
+      <Link to="/gestures" className={`button ${getLinkClass("/gestures")}`}>
         Gestures
       </Link>
-      <Link to="animationControls" className="button">
+      <Link
+        to="/animationControls"
+        className={`button ${getLinkClass("/animationControls")}`}
+      >
         Animation Controls
       </Link>
-      <Link to="viewAnimations" className="button">
+      <Link
+        to="/viewAnimations"
+        className={`button ${getLinkClass("/viewAnimations")}`}
+      >
         View Animations
       </Link>
-      <Link to="scrollAnimations" className="button">
+      <Link
+        to="/scrollAnimations"
+        className={`button ${getLinkClass("/scrollAnimations")}`}
+      >
         Scroll Animations
       </Link>
-      <Link to="advanceScrollAnimations" className="button">
+      <Link
+        to="/advanceScrollAnimations"
+        className={`button ${getLinkClass("/advanceScrollAnimations")}`}
+      >
         Advance Scroll Animations
       </Link>
-      <Link to="/tinderSwipeCards" className="button">
+      <Link
+        to="/tinderSwipeCards"
+        className={`button ${getLinkClass("/tinderSwipeCards")}`}
+      >
         Tinder Swipe Cards
       </Link>
     </nav>
