140b34d94aa3821b630906623d959020953342ec
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-08-27 09:44:36 +0500

    Updated text styles

--- src/components/404-not-found.tsx
+++ src/components/404-not-found.tsx
@@ -1,8 +1,10 @@
 export const NotFound = () => {
   return (
-    <div className="flex items-center justify-center text-lg">
-      404. Page not found
-      <a href="/">Back to home</a>
+    <div className="flex gap-2 items-center justify-center text-lg">
+      404. Page not found{" "}
+      <a href="/" className="underline">
+        Back to home
+      </a>
     </div>
   );
 };
